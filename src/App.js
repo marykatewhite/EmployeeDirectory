c9523361e3f2c6d2758876292281c161f468a094
MaryEtak <marykatewhite@gmail.com>
2020-03-29 11:55:02 -0400

    renders employee cards on the page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//  import './App.css';
+import Navbar from "./components/Navbar.js";
+import Search from "./components/Search.js";
+import Main from "./components/Main.js";
+import Table from "./components/Table.js";
+import employees from "./employees.json";
+import EmployeeCard from "./components/EmployeeCard.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees
+  };
+
+  // removeFriend = id => {
+  // Filter this.state.friends for friends with an id not equal to the id being removed
+  // const friends = this.state.friends.filter(friend => friend.id !== id);
+  // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Search />
+        
+        {this.state.employees.map(employee => (
+          <EmployeeCard
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            role={employee.role}
+            salary={employee.salary}
+            office={employee.office}
+          />
+        ))}
+
+      </div>
+    );
+  }
 }
 
 export default App;
